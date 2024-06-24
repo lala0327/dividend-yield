ac2f622d75aa0541bc827f8122d360b6f07b35f3
lala <lala@program.com.tw>
2024-06-24 11:10:24 +0800

    feat: 畫面調整

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,67 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import TextField from '@mui/material/TextField';
 import './App.css'
+import { Button } from '@mui/material';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isError, setIsError] = useState(false)
+    // 股價// 股息// 殖利率
+  const [answer, setAnswer] = useState(0)
+  const [anserText, setAnserText] = useState("")
 
+  {/* 
+    股價A=B×100÷C
+    股息B=A×C÷100
+    殖利率%C=B×100÷A 
+  */}
+  const calculate = ()=>{
+    const inputArray = [!!(document.getElementById('sharePrice') as HTMLInputElement).value, !!(document.getElementById('dividends') as HTMLInputElement).value, !!(document.getElementById('dividendsYield') as HTMLInputElement).value]
+    const num = inputArray.filter(function(value) {
+      return value === true;
+    });
+    if(num.length === 2){
+      setIsError(false)
+      let A = 0
+      let B = 0
+      let C = 0
+      let ans = 0
+      if(!!(document.getElementById('sharePrice') as HTMLInputElement).value === false){
+        B = parseInt((document.getElementById('dividends') as HTMLInputElement).value)
+        C = parseInt((document.getElementById('dividendsYield') as HTMLInputElement).value)
+        ans = B*100/C
+        setAnserText('股價')
+      }
+      if(!!(document.getElementById('dividends') as HTMLInputElement).value === false){
+        A = parseInt((document.getElementById('sharePrice') as HTMLInputElement).value)
+        C = parseInt((document.getElementById('dividendsYield') as HTMLInputElement).value)
+        ans = A*C/100
+        setAnserText('股息')
+      }
+      if(!!(document.getElementById('dividendsYield') as HTMLInputElement).value === false){
+        A = parseInt((document.getElementById('sharePrice') as HTMLInputElement).value)
+        B = parseInt((document.getElementById('dividends') as HTMLInputElement).value)
+        ans = B*100/A
+        setAnserText('殖利率')
+      }
+      setAnswer(parseInt(ans.toFixed(2)))
+    }else{
+      setIsError(true)
+      setAnswer(0)
+      setAnserText("")
+    }
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <p style={{fontSize:'36px',fontWeight:'700',margin:"0px"}}>股票殖利率計算機</p>
+      <p style={{margin:"0px"}}>請輸入隨機兩格輸入框內容<br/>即可計算出另一參數值</p>
+      <div style={{display:'flex',justifyContent:'center',alignItems:'center',flexDirection:'column',width:'80%', maxWidth: '550px'}}>
+        <TextField id="sharePrice" error={isError} type="number" label="股價" variant="outlined" sx={{width:'100%',margin:'15px 0px'}}/>
+        <TextField id="dividends" error={isError} type="number" label="股息" variant="outlined" sx={{width:'100%',margin:'15px 0px'}} />
+        <TextField id="dividendsYield" error={isError} type="number" label="殖利率" variant="outlined" sx={{width:'100%',margin:'15px 0px'}} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {isError?<p style={{fontSize:'20px',fontWeight:'700', margin:"0px", color:'#d32f2f'}}>請輸入兩格輸入框內容</p>:''}
+      <Button onClick={calculate} variant="contained" size="large" sx={{fontSize:'20px',fontWeight:'700', margin:"10px 0px"}}>計算結果</Button>
+      <p style={{fontSize:'20px',fontWeight:'700',margin:"10px 0px", border:"1px solid black", width:'80%', maxWidth: '550px'}}>{anserText?`${anserText}: ${answer}`:''}</p>
     </>
   )
 }
