f92a67fbbf0aec5edc94958876b6dba1025cf3c4
lala <sss2527tw@gmail.com>
2024-07-14 17:20:17 +0800

    fix

--- src/page/MainPage.tsx
+++ src/page/MainPage.tsx
@@ -8,10 +8,12 @@ function MainPage() {
     <>
       {/* <DrawerCompoment/> */}
       <div style={{width:'100%', height:'calc(100% - 64px)'}}>
-        <p>使用者請匯錢解鎖</p>
+        <p style={{fontSize:'72px', color:'red', fontWeight:'700', margin:'20px 0px'}}>404 Error</p>
+        <p style={{fontSize:'32px'}}>作者缺錢中</p>
+        <p style={{fontSize:'32px', fontWeight:'700'}}>使用者請匯錢解鎖</p>
         {/* <Route /> */}
       </div>
-      <p style={{position:'absolute',left:'10px',bottom:'10px'}}>v1.0.4</p>
+      <p style={{position:'absolute',left:'10px',bottom:'10px'}}>v1.0.5</p>
     </>
   )
 }
